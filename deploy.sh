8c936dd4f12f5e0e9c31842d055eb35bfee81643
Masaki Kobayashi <mkobayashime@gmail.com>
2020-06-08 16:42:02 +0900

    Add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+DOTPATH=~/dotfiles
+
+if [ $? -ne 0 ]; then
+    die "not found: $DOTPATH"
+fi
+
+for f in .??*
+do
+    [ "$f" = ".git" ] && continue
+
+    ln -snfv "$DOTPATH/$f" "$HOME/$f"
+done
+
