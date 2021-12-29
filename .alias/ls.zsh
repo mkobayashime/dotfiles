ac87807d1a83d438a8a8e55459567544aa1e82ec
Masaki Kobayashi <contact@mkobayashi.me>
2021-12-29 23:15:28 +0900

    Make `ls` aliases global just as before

--- .alias/ls.zsh
+++ .alias/ls.zsh
@@ -1,20 +1,19 @@
-alias ls='ls -v --color=auto --group-directories-first'
+alias -g ls='ls -v --color=auto --group-directories-first'
 
 # BSD
 ls --help &> /dev/null
 if [[ $? -ne 0 ]]; then
-  alias ls='ls -G'
+  alias -g ls='ls -G'
 fi
 
-alias ls1='ls -1'
+alias -g ls1='ls -1'
 
 declare -A abbrs=(
   [ll]='ls -alhF'
   [la]='ls -A'
-  [l]='ls -CF'
 )
 for key value in ${(@kv)abbrs}
 do
-  alias "$key"="$value"
-  alias "${key}1"="$value -1"
+  alias -g "$key"="$value"
+  alias -g "${key}1"="$value -1"
 done
