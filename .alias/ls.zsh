32e42827d09cbaffc9e22a7c5012c01be7cd2116
Masaki Kobayashi <contact@mkobayashi.me>
2021-12-29 23:11:41 +0900

    Add one-line output version to `ls` aliases

--- .alias/ls.zsh
+++ .alias/ls.zsh
@@ -1,10 +1,20 @@
-alias ls="ls -v --color=auto --group-directories-first"
-alias -g ll="ls -alhF"
-alias -g la="ls -A"
-alias -g l="ls -CF"
+alias ls='ls -v --color=auto --group-directories-first'
 
 # BSD
 ls --help &> /dev/null
 if [[ $? -ne 0 ]]; then
-  alias ls="ls -G"
+  alias ls='ls -G'
 fi
+
+alias ls1='ls -1'
+
+declare -A abbrs=(
+  [ll]='ls -alhF'
+  [la]='ls -A'
+  [l]='ls -CF'
+)
+for key value in ${(@kv)abbrs}
+do
+  alias "$key"="$value"
+  alias "${key}1"="$value -1"
+done
