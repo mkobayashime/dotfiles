de994f5be7385124bf6b6eb6ba2c99aeeae325eb
Masaki Kobayashi <contact@mkobayashi.me>
2022-01-01 22:51:22 +0900

    Create symlinks for `.config/*` files as well

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,9 @@ if [ $? -ne 0 ]; then
     die "not found: $DOTPATH"
 fi
 
-for f in .??*
+for f in .??* .config/*
 do
     [ "$f" = ".git" -o "$f" = ".gitignore" -o "$f" = ".gitmodules" ] && continue
 
     ln -snfv "$DOTPATH/$f" "$HOME/$f"
 done
-
