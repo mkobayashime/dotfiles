13717fdb8f503504878941e0a916941144bd6a37
Masaki Kobayashi <contact@mkobayashi.me>
2022-01-04 20:46:26 +0900

    Fix improper symbolic linking in deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,11 @@ if [ $? -ne 0 ]; then
     die "not found: $DOTPATH"
 fi
 
+mkdir -p $HOME/.config
+
 for f in .??* .config/*
 do
-    [ "$f" = ".git" -o "$f" = ".gitignore" -o "$f" = ".gitmodules" ] && continue
+    [ "$f" = ".git" -o "$f" = ".gitignore" -o "$f" = ".gitmodules" -o "$f" = ".config" ] && continue
 
     ln -snfv "$DOTPATH/$f" "$HOME/$f"
 done
