a38acf25cd8aa1f20256b0fcad49ab406f429e20
Masaki Kobayashi <contact@mkobayashi.me>
2022-02-11 03:56:05 +0900

    Define colors for `nvim-ts-rainbow`

--- .config/nvim/plugins/nvim-treesitter.lua
+++ .config/nvim/plugins/nvim-treesitter.lua
@@ -4,6 +4,22 @@ require('nvim-treesitter.configs').setup {
     enable = true,
   },
   rainbow = {
-    enable = true
+    enable = true,
+    colors = {
+      -- https://github.com/joshdick/onedark.vim/blob/main/colors/onedark.vim
+      '#98c379', -- green
+      '#c678dd', -- magenta
+      '#e5c07b', -- yellow
+      '#e06c75', -- light red
+      '#61afef', -- blue
+    },
+    termcolors = {
+      -- https://github.com/joshdick/onedark.vim/blob/main/autoload/onedark.vim
+      '114', -- green
+      '170', -- purple
+      '173', -- dark_yellow
+      '204', -- red
+      '39', -- blue
+    }
   }
 }
