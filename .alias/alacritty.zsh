83bb72cd56576a6bce69f07f58ec47c64615cf2d
Masaki Kobayashi <contact@mkobayashi.me>
2022-02-19 01:16:07 +0900

    Make background color in Alacritty darkder

--- .alias/alacritty.zsh
+++ .alias/alacritty.zsh
@@ -1,2 +1,2 @@
-alias alacritty.transparent.disable="sed -i 's/opacity: 0.9/opacity: 1/' ~/dotfiles/.alacritty.yml"
-alias alacritty.transparent="sed -i 's/opacity: 1/opacity: 0.9/' ~/dotfiles/.alacritty.yml"
+alias alacritty.transparent.disable="sed -i 's/opacity: 0.85/opacity: 1/' ~/dotfiles/.alacritty.yml"
+alias alacritty.transparent="sed -i 's/opacity: 1/opacity: 0.85/' ~/dotfiles/.alacritty.yml"
