c72be08e5c679f6df6b1927ec8f77c59a8e86ce9
Masaki Kobayashi <contact@mkobayashi.me>
2022-02-20 02:24:44 +0900

    Close Telescope with `<C-c>`  in normal mode as well

--- .config/nvim/plugins/telescope.lua
+++ .config/nvim/plugins/telescope.lua
@@ -6,7 +6,10 @@ require('telescope').setup {
         ["<C-p>"] = false,
         ["<C-j>"] = "move_selection_next",
         ["<C-k>"] = "move_selection_previous",
-      }
+      },
+      n = {
+        ["<C-c>"] = "close"
+      },
     }
   }
 }
