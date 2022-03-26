10a4fb7caf65cae96332b2a2025e046efc514ee0
Masaki Kobayashi <contact@mkobayashi.me>
2022-03-27 03:24:20 +0900

    Use `nvim-treesitter-context`

---
+++ .config/nvim/plugins/nvim-treesitter-context.lua
@@ -0,0 +1,14 @@
+require('treesitter-context').setup({
+  patterns = {
+    default = {
+      'class',
+      'function',
+      'method',
+      'for',
+      'while',
+      'if',
+      'switch',
+      'case',
+    }
+  }
+})
