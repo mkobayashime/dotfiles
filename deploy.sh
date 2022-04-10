ad4029571645528fdf3dd7078a375af1242c0ccb
Masaki Kobayashi <contact@mkobayashi.me>
2022-04-10 12:47:26 +0900

    Fix lint warnings in deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,15 @@
 
 DOTPATH=~/dotfiles
 
-if [[ ! `pwd` -ef "$DOTPATH" ]]; then
+if [[ ! $(pwd) -ef "$DOTPATH" ]]; then
   echo "Locate this repository in $DOTPATH"
   exit 1
 fi
 
-mkdir -p $HOME/.config
+mkdir -p "$HOME/.config"
 
-for f in .??* .config/*
-do
-    [ "$f" = ".git" -o "$f" = ".gitignore" -o "$f" = ".gitmodules" -o "$f" = ".config" ] && continue
+for f in .??* .config/*; do
+  [[ "$f" = ".git" ]] || [[ "$f" = ".gitignore" ]] || [[ "$f" = ".gitmodules" ]] || [[ "$f" = ".config" ]] && continue
 
-    ln -snfv "$DOTPATH/$f" "$HOME/$f"
+  ln -snfv "$DOTPATH/$f" "$HOME/$f"
 done
