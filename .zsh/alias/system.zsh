9b0576bd854a79f5d0a6503d603f89fed70f0de8
Masaki Kobayashi <contact@mkobayashi.me>
2022-05-16 19:12:34 +0900

    Alias `diff` to `icdiff` if available

--- .zsh/alias/system.zsh
+++ .zsh/alias/system.zsh
@@ -3,3 +3,4 @@ alias fusermount="fusermount -u"
 alias rimraf="rm -rf"
 alias pulseaudiorestart="pulseaudio --check && pulseaudio --kill && pulseaudio --start"
 alias maek="make" # typo tolerance
+hascommand icdiff && alias diff='icdiff'
