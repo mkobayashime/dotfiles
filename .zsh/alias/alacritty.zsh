9d5da58d0dcaa4ee335470b79eaaa58ea74fca77
Masaki Kobayashi <contact@mkobayashi.me>
2022-05-16 14:43:40 +0900

    Change transparency of Alacritty window

--- .zsh/alias/alacritty.zsh
+++ .zsh/alias/alacritty.zsh
@@ -1,2 +1,2 @@
-alias alacritty.transparent.disable="sed -i 's/opacity: 0.85/opacity: 1/' ~/dotfiles/.alacritty.yml"
-alias alacritty.transparent="sed -i 's/opacity: 1/opacity: 0.85/' ~/dotfiles/.alacritty.yml"
+alias alacritty.transparent.disable="sed -i 's/opacity: 0.8/opacity: 1/' ~/dotfiles/.alacritty.yml"
+alias alacritty.transparent="sed -i 's/opacity: 1/opacity: 0.8/' ~/dotfiles/.alacritty.yml"
