498c005f8533ba905dc9d478e15ad8b42ad62b23
Masaki Kobayashi <contact@mkobayashi.me>
2022-08-18 21:11:11 +0900

    Update Alacritty related aliases

--- .config/zsh/alias/alacritty.zsh
+++ .config/zsh/alias/alacritty.zsh
@@ -1,2 +1,2 @@
-alias alacritty.transparent.disable="sed -i 's/opacity: 0.8/opacity: 1/' ~/dotfiles/.alacritty.yml"
-alias alacritty.transparent="sed -i 's/opacity: 1/opacity: 0.8/' ~/dotfiles/.alacritty.yml"
+alias alacritty.transparent.disable="sed -i 's/opacity: 0.8/opacity: 1/' ~/dotfiles/.config/alacritty/alacritty.yml"
+alias alacritty.transparent="sed -i 's/opacity: 1/opacity: 0.8/' ~/dotfiles/.config/alacritty/alacritty.yml"
