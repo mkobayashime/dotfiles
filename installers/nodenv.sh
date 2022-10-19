0d08f5e8a441c28e32acf3feabf8cb298cb4cb16
Masaki Kobayashi <contact@mkobayashi.me>
2022-10-20 00:12:35 +0900

    Stop installing Node version

--- installers/nodenv.sh
+++ installers/nodenv.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-node_version=16.15.1
-
 if [[ -d ~/.config/nodenv ]]; then
   echo 'nodenv already installed'
   exit 0
@@ -14,6 +12,3 @@ cd plugins || exit 1
 git clone https://github.com/nodenv/node-build.git
 git clone https://github.com/ouchxp/nodenv-nvmrc.git
 git clone https://github.com/pine/nodenv-yarn-install.git
-
-nodenv install "$node_version"
-nodenv global "$node_version"
