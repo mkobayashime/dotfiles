4cb7f99a713690310eeb520a3003f344193e4195
Masaki Kobayashi <contact@mkobayashi.me>
2022-11-10 17:13:52 +0900

    Automatically install Node version with nodenv again

--- installers/nodenv.sh
+++ installers/nodenv.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+node_version=18.12.1
+
 if [[ -d ~/.config/nodenv ]]; then
   echo 'nodenv already installed'
   exit 0
@@ -12,3 +14,10 @@ cd plugins || exit 1
 git clone https://github.com/nodenv/node-build.git
 git clone https://github.com/ouchxp/nodenv-nvmrc.git
 git clone https://github.com/pine/nodenv-yarn-install.git
+
+export PATH="$HOME/.config/nodenv/bin:$PATH"
+export NODENV_ROOT="$HOME/.config/nodenv"
+eval "$(nodenv init -)"
+
+nodenv install "$node_version"
+nodenv global "$node_version"
