88f8f61051fa6d42a68195c81065b40a89bbbc3a
Masaki Kobayashi <contact@mkobayashi.me>
2022-12-08 22:49:13 +0900

    Add Rust installer

---
+++ installers/rust.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+source ~/.config/bash/functions/__hascommand
+
+if __hascommand rustup; then
+  echo 'Rust already installed'
+  exit 0
+fi
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
