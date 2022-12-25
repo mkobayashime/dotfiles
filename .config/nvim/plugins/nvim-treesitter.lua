75dc25b32f906c6e608e685b7139ff96eed05b1f
Masaki Kobayashi <contact@mkobayashi.me>
2022-12-25 22:32:57 +0900

    Disable highlighting with nvim-treesitter in files over 1MB

--- .config/nvim/plugins/nvim-treesitter.lua
+++ .config/nvim/plugins/nvim-treesitter.lua
@@ -26,6 +26,13 @@ require('nvim-treesitter.configs').setup {
   },
   highlight = {
     enable = true,
+    disable = function(lang, buf)
+        local max_filesize = 1024 * 1024 -- 1MB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+            return true
+        end
+    end,
   },
   autotag = {
     enable = true,
