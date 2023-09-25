cb0818f125d3d6bcf8211d1f6d64ed8bfdcfbba8
Masaki Kobayashi <contact@mkobayashi.me>
2023-09-25 21:14:41 +0900

    Update search related highlight

--- .config/nvim-lua/lua/highlight.lua
+++ .config/nvim-lua/lua/highlight.lua
@@ -13,6 +13,7 @@ vim.api.nvim_create_autocmd({ "ColorScheme" }, {
 
     -- basic
     vim.api.nvim_set_hl(0, "IncSearch", { link = "DiffAdd" })
+    vim.api.nvim_set_hl(0, "Search", { link = "Visual" })
 
     -- coc
     vim.api.nvim_set_hl(0, "NormalFloat", {})
