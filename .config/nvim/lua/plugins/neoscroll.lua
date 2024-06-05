56440f4e43cf8ca953c3d16c705ecea08a184369
Masaki Kobayashi <contact@mkobayashi.me>
2024-06-05 14:17:31 +0900

    Disable smooth scroll in `<C-y>`/`<C-e>`

--- .config/nvim/lua/plugins/neoscroll.lua
+++ .config/nvim/lua/plugins/neoscroll.lua
@@ -1,11 +1,16 @@
-require("neoscroll").setup()
+require("neoscroll").setup({
+  mappings = {
+    "<C-u>", "<C-d>", "<C-b>", "<C-f>",
+    "zt", "zz", "zb",
+  },
+})
 
 require("neoscroll.config").set_mappings({
   ["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "150" } },
   ["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "150" } },
-  ["<C-y>"] = { "scroll", { "-0.10", "false", "100" } },
+  -- ["<C-y>"] = { "scroll", { "-0.10", "false", "100" } },
   ["<C-k>"] = { "scroll", { "-0.10", "false", "100" } },
-  ["<C-e>"] = { "scroll", { "0.10", "false", "100" } },
+  -- ["<C-e>"] = { "scroll", { "0.10", "false", "100" } },
   ["<C-j>"] = { "scroll", { "0.10", "false", "100" } },
   ["zt"]    = { "zt", { "150" } },
   ["zz"]    = { "zz", { "150" } },
