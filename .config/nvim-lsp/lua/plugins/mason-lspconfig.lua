507018760ef186bd2067b47eee641c1f049e74f3
Masaki Kobayashi <contact@mkobayashi.me>
2025-01-03 02:51:58 +0900

    Add some LSP servers

--- .config/nvim-lsp/lua/plugins/mason-lspconfig.lua
+++ .config/nvim-lsp/lua/plugins/mason-lspconfig.lua
@@ -1,11 +1,14 @@
 require("mason-lspconfig").setup({
   ensure_installed = {
     "bashls",
+    "biome",
     "cssls",
     "eslint",
     "html",
     "jsonls",
+    "lua_ls",
     "ts_ls",
+    "yamlls",
   },
 })
 
