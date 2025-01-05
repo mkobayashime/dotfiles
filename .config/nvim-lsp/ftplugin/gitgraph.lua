2e7136bc15b9f5572c10006a92a12453b88adf11
Masaki Kobayashi <contact@mkobayashi.me>
2025-01-05 13:19:07 +0900

    Make keymaps for `gitgraph` buffer-local

--- .config/nvim-lsp/ftplugin/gitgraph.lua
+++ .config/nvim-lsp/ftplugin/gitgraph.lua
@@ -2,8 +2,9 @@ local neoscroll = require("neoscroll")
 
 vim.keymap.set({ "n", "v" }, "d", function()
   neoscroll.ctrl_d({ duration = 250 })
-end, { remap = false, nowait = true })
+end, { remap = false, nowait = true, buffer = true })
 vim.keymap.set({ "n", "v" }, "u", function()
   neoscroll.ctrl_u({ duration = 250 })
-end, { remap = false, nowait = true })
-vim.keymap.set({ "n" }, "q", ":q<CR>")
+end, { remap = false, nowait = true, buffer = true })
+
+vim.keymap.set({ "n" }, "q", ":q<CR>", { remap = false, buffer = true })
