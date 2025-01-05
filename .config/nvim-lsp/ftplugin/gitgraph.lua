f857a495be5ec2ed09841af6fb3d135244138ccd
Masaki Kobayashi <contact@mkobayashi.me>
2025-01-05 13:57:34 +0900

    Snap to odd lines in gitgraph

--- .config/nvim-lsp/ftplugin/gitgraph.lua
+++ .config/nvim-lsp/ftplugin/gitgraph.lua
@@ -1,5 +1,9 @@
 local neoscroll = require("neoscroll")
 
+local map_options = { remap = false, buffer = true }
+
+vim.keymap.set({ "n" }, "q", ":q<CR>", map_options)
+
 vim.keymap.set({ "n", "v" }, "d", function()
   neoscroll.ctrl_d({ duration = 250 })
 end, { remap = false, nowait = true, buffer = true })
@@ -7,4 +11,20 @@ vim.keymap.set({ "n", "v" }, "u", function()
   neoscroll.ctrl_u({ duration = 250 })
 end, { remap = false, nowait = true, buffer = true })
 
-vim.keymap.set({ "n" }, "q", ":q<CR>", { remap = false, buffer = true })
+vim.keymap.set({ "n", "v" }, "j", "2j", map_options)
+vim.keymap.set({ "n", "v" }, "k", "2k", map_options)
+
+local augroup_gitgraph = vim.api.nvim_create_augroup("_gitgraph", { clear = true })
+
+vim.api.nvim_create_autocmd({ "CursorMoved" }, {
+  group = augroup_gitgraph,
+  buffer = 0,
+  callback = function()
+    ---@diagnostic disable-next-line: deprecated
+    local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+
+    if row % 2 == 0 then
+      vim.api.nvim_win_set_cursor(0, { row - 1, col })
+    end
+  end,
+})
