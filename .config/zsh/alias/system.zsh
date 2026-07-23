ff4c3be5c997ac71285f740c57acc32b214effe7
Masaki Kobayashi <contact@mkobayashi.me>
2026-07-24 00:24:44 +0900

    Use `delta` instead of `icdiff`

--- .config/zsh/alias/system.zsh
+++ .config/zsh/alias/system.zsh
@@ -4,4 +4,4 @@ alias fusermount="fusermount -u"
 alias rimraf="rm -rf"
 alias pulseaudiorestart="pulseaudio --check && pulseaudio --kill && pulseaudio --start"
 alias maek="make" # typo tolerance
-hascommand icdiff && alias diff='icdiff'
+hascommand delta && alias diff='delta -s'
